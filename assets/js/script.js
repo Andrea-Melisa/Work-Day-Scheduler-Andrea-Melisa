fbbdcbd99c345b9de6e3e6bc5eb7355636b242d2
Melisa <andream_dearth@hotmail.com>
2024-01-05 20:46:56 +0100

    Adding the the current day at the top of the calende with dayjs at the js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,17 @@
+//Display the current day at the top of the calender when a user opens the planner.
+
+const date = dayjs().format('dddd, D MMM YYYY');
+console.log(date);
+$("#currentDay").text(date);
+
 // function to save to local storage when button is clicked
 	// get the values
 	// store them
-    ​
+    
     // function to update classes hours change
         // get current time
         // comptare against those in your time blocks
         // update the classes
-    ​
+
     // remember to call your update function on page load
     // remember to also get values from local storage and set them on page load
