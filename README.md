bafa99e097f525c2f66dd623fc8ceea80edc9cc9
Melisa <andream_dearth@hotmail.com>
2024-01-09 01:05:28 +0000

    Adding the rest of the time-blocks, adding the disk image, adding the function to assign the background coulour

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Work-Day-Scheduler-Andrea-Melisa
 Create a simple calendar application that allows a user to save events for each hour of the day.
+
+
+https://icons8.com/icon/21394/save
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,49 @@
 //Display when a user opens the planner.
 
-const date = dayjs().format('dddd, D MMM YYYY');
-console.log(date);
-$("#currentDay").text(date);
+$(function() {
+    const date = dayjs().format('dddd, D MMM YYYY');
+    console.log(date);
+    $("#currentDay").text(date);
+
+    assignBackgroundColour()
+});
+
+
+//Get the current time:
+    // const currentTime = dayjs().format('hA');
+    // console.log(currentTime);
+
+
+ // Assign background colour depending on the time-block hour: past, present, or future
+//  let textArea = $('.eventEnter');
+//  var hour = $('.hour').attr('id');
+//  (function assignBackgroundColour() {
+//     console.log(hour)
+//     if (hour < dayjs().hour()) {
+//         textArea.addClass("past");
+//     } else if (hour === dayjs().hour()) {
+//         textArea.addClass("present");
+//     } else {
+//         textArea.addClass("future");
+//     }
+// }());
+
+
+
+function assignBackgroundColour() {
+    const hourBlocks = $('.hour');  // select all blocks with class 'hour' to know the hour frame
+    const currentTime = parseInt(dayjs().hour());  // get the current time in 24 hour format
+    hourBlocks.each(function() {
+        let rowHour = parseInt($(this).attr('id')); // extract their IDs
+        if (rowHour < currentTime) {   // compare IDs to currentTime
+            $(this).next().addClass('past');  // assign styling classes
+        } else if (rowHour === currentTime) {
+            $(this).next().addClass('present');
+        } else if (rowHour > currentTime) {
+            $(this).next().addClass('future');
+        };
+    });
+};
 
 // function to save to local storage when button is clicked
 	// get the values
