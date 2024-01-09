5b7edf0cdfe6d7dedb4a7bcef4f7181f2004440f
Melisa <andream_dearth@hotmail.com>
2024-01-09 16:28:29 +0000

    Adding the function to save to local storage when button clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,31 @@
 //Display when a user opens the planner.
 
 $(function() {
+
     const date = dayjs().format('dddd, D MMM YYYY');
     console.log(date);
     $("#currentDay").text(date);
 
     assignBackgroundColour()
+
+
+
 });
 
+// Update the hour constantly
 
-//Get the current time:
-    // const currentTime = dayjs().format('hA');
-    // console.log(currentTime);
+function checkTime() {
 
-
- // Assign background colour depending on the time-block hour: past, present, or future
-//  let textArea = $('.eventEnter');
-//  var hour = $('.hour').attr('id');
-//  (function assignBackgroundColour() {
-//     console.log(hour)
-//     if (hour < dayjs().hour()) {
-//         textArea.addClass("past");
-//     } else if (hour === dayjs().hour()) {
-//         textArea.addClass("present");
-//     } else {
-//         textArea.addClass("future");
-//     }
-// }());
+    const timer = setInterval(() => {
+        assignColors;
+    }, 1000);
+}
 
 
+// Assign backgound coulour
 
 function assignBackgroundColour() {
+
     const hourBlocks = $('.hour');  // select all blocks with class 'hour' to know the hour frame
     const currentTime = parseInt(dayjs().hour());  // get the current time in 24 hour format
     hourBlocks.each(function() {
@@ -45,14 +40,15 @@ function assignBackgroundColour() {
     });
 };
 
-// function to save to local storage when button is clicked
-	// get the values
-	// store them
-    
-    // function to update classes hours change
-        // get current time
-        // comptare against those in your time blocks
-        // update the classes
+// Function to save to local storage when button is clicked
 
-    // remember to call your update function on page load
-    // remember to also get values from local storage and set them on page load
+$('.saveBtn').on('click', function() {
+
+    const timeBlock = $(this).closest('.time-block'); // Find the closest time-block element
+    const timeBlockId = timeBlock.attr('id'); // Extract the time block's ID
+    //  alert(timeBlockId)
+    const textArea = timeBlock.find('.eventEnter');// Find the textarea within the time block
+    const eventText = textArea.val(); // Get the value of the textarea
+    localStorage.setItem(timeBlockId, JSON.stringify(eventText)); // Save the event to local storage
+});
+
